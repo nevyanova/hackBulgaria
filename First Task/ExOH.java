56c8b89fa1f9d89aae6463fe6b48146a8013cb84
faraonq <nikolai.ivanov17@gmail.com>
2014-09-25 23:43:34 +0300

    Task

---
+++ First Task/ExOH.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class ExOH {
+	
+	public boolean ExOH(String str){
+		
+	int count = str.length() - str.replace("x", "").length();
+	int count1 = str.length() - str.replace("o", "").length();
+	
+	if(count == count1){
+		return true;
+	}else 
+		return false;
+	
+	}
+	public static void main (String[]Args){
+		
+		ExOH obj = new ExOH ();
+		boolean tr = obj.ExOH("xoxoox");
+		boolean fs = obj.ExOH("oooxoo");
+		System.out.println(tr);
+		System.out.println(fs);
+		
+	}
+
+} 
